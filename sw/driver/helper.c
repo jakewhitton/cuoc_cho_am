842195ab9b73f967a7ef2a153f9107e7b426bbba
Jake Whitton <jwhitton@alum.mit.edu>
2024-08-14 15:28:38 -0400

    Remove unneeded code from helper file in driver

--- sw/driver/helper.c
+++ sw/driver/helper.c
@@ -1,5 +1,3 @@
-#include <linux/kernel.h>
-#include <linux/module.h>
 #include <linux/random.h>
 
 #include "helper.h"
@@ -10,5 +8,3 @@ int get_random_int(void)
 	get_random_bytes((void*)&random, sizeof(random));
 	return random % 6;
 }
-
-MODULE_LICENSE("GPL v2");
