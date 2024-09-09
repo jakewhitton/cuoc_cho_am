6093039ac254b3ddc7be1e6eeacb483ac4d42791
Jake Whitton <jwhitton@alum.mit.edu>
2024-09-09 17:17:35 -0400

    Clean up mixer logic in driver

--- sw/driver/mixer.c
+++ sw/driver/mixer.c
@@ -5,15 +5,7 @@
 
 #include "device.h"
 
-#define CCO_VOLUME(xname, xindex, addr) \
-{ .iface = SNDRV_CTL_ELEM_IFACE_MIXER, \
-  .access = SNDRV_CTL_ELEM_ACCESS_READWRITE | SNDRV_CTL_ELEM_ACCESS_TLV_READ, \
-  .name = xname, .index = xindex, \
-  .info = cco_volume_info, \
-  .get = cco_volume_get, .put = cco_volume_put, \
-  .private_value = addr, \
-  .tlv = { .p = db_scale_cco } }
-
+/*===================================Volume===================================*/
 static int cco_volume_info(struct snd_kcontrol *kcontrol,
                            struct snd_ctl_elem_info *uinfo)
 {
@@ -49,30 +41,44 @@ static int cco_volume_put(struct snd_kcontrol *kcontrol,
         left = mixer_volume_level_min;
     if (left > mixer_volume_level_max)
         left = mixer_volume_level_max;
+
     right = ucontrol->value.integer.value[1];
     if (right < mixer_volume_level_min)
         right = mixer_volume_level_min;
     if (right > mixer_volume_level_max)
         right = mixer_volume_level_max;
+
     spin_lock_irq(&cco->mixer_lock);
     change = cco->mixer_volume[addr][0] != left ||
              cco->mixer_volume[addr][1] != right;
     cco->mixer_volume[addr][0] = left;
     cco->mixer_volume[addr][1] = right;
     spin_unlock_irq(&cco->mixer_lock);
+
     return change;
 }
 
 static const DECLARE_TLV_DB_SCALE(db_scale_cco, -4500, 30, 0);
 
-#define CCO_CAPSRC(xname, xindex, addr) \
-{ .iface = SNDRV_CTL_ELEM_IFACE_MIXER, .name = xname, .index = xindex, \
-  .info = cco_capsrc_info, \
-  .get = cco_capsrc_get, .put = cco_capsrc_put, \
-  .private_value = addr }
+#define CCO_VOLUME(xname, xindex, addr)                  \
+{                                                        \
+    .iface         = SNDRV_CTL_ELEM_IFACE_MIXER,         \
+    .access        = ( SNDRV_CTL_ELEM_ACCESS_READWRITE   \
+                     | SNDRV_CTL_ELEM_ACCESS_TLV_READ ), \
+    .name          = xname,                              \
+    .index         = xindex,                             \
+    .info          = cco_volume_info,                    \
+    .get           = cco_volume_get,                     \
+    .put           = cco_volume_put,                     \
+    .private_value = addr,                               \
+    .tlv           = {                                   \
+        .p = db_scale_cco                                \
+    }                                                    \
+}
+/*============================================================================*/
 
-#define cco_capsrc_info    snd_ctl_boolean_stereo_info
 
+/*===================================Capsrc===================================*/
 static int cco_capsrc_get(struct snd_kcontrol *kcontrol,
                           struct snd_ctl_elem_value *ucontrol)
 {
@@ -87,7 +93,7 @@ static int cco_capsrc_get(struct snd_kcontrol *kcontrol,
 }
 
 static int cco_capsrc_put(struct snd_kcontrol *kcontrol,
-						  struct snd_ctl_elem_value *ucontrol)
+                          struct snd_ctl_elem_value *ucontrol)
 {
     struct cco_device *cco = snd_kcontrol_chip(kcontrol);
     int change, addr = kcontrol->private_value;
@@ -104,6 +110,20 @@ static int cco_capsrc_put(struct snd_kcontrol *kcontrol,
     return change;
 }
 
+#define CCO_CAPSRC(xname, xindex, addr)           \
+{                                                 \
+    .iface         = SNDRV_CTL_ELEM_IFACE_MIXER,  \
+    .name          = xname,                       \
+    .index         = xindex,                      \
+    .info          = snd_ctl_boolean_stereo_info, \
+    .get           = cco_capsrc_get,              \
+    .put           = cco_capsrc_put,              \
+    .private_value = addr                         \
+}
+/*============================================================================*/
+
+
+/*===================================I/O Box==================================*/
 static int cco_iobox_info(struct snd_kcontrol *kcontrol,
                           struct snd_ctl_elem_info *info)
 {
@@ -135,44 +155,44 @@ static int cco_iobox_put(struct snd_kcontrol *kcontrol,
         cco->iobox = value->value.enumerated.item[0];
 
         if (cco->iobox) {
-            cco->cd_volume_ctl->vd[0].access &=
-                ~SNDRV_CTL_ELEM_ACCESS_INACTIVE;
-            cco->cd_switch_ctl->vd[0].access &=
-                ~SNDRV_CTL_ELEM_ACCESS_INACTIVE;
+            cco->cd_volume_ctl->vd[0].access &= ~SNDRV_CTL_ELEM_ACCESS_INACTIVE;
+            cco->cd_switch_ctl->vd[0].access &= ~SNDRV_CTL_ELEM_ACCESS_INACTIVE;
         } else {
-            cco->cd_volume_ctl->vd[0].access |=
-                SNDRV_CTL_ELEM_ACCESS_INACTIVE;
-            cco->cd_switch_ctl->vd[0].access |=
-                SNDRV_CTL_ELEM_ACCESS_INACTIVE;
+            cco->cd_volume_ctl->vd[0].access |= SNDRV_CTL_ELEM_ACCESS_INACTIVE;
+            cco->cd_switch_ctl->vd[0].access |= SNDRV_CTL_ELEM_ACCESS_INACTIVE;
         }
 
         snd_ctl_notify(cco->card, SNDRV_CTL_EVENT_MASK_INFO,
-                   &cco->cd_volume_ctl->id);
+                       &cco->cd_volume_ctl->id);
         snd_ctl_notify(cco->card, SNDRV_CTL_EVENT_MASK_INFO,
-                   &cco->cd_switch_ctl->id);
+                       &cco->cd_switch_ctl->id);
     }
 
     return changed;
 }
 
+#define CCO_IOBOX(xname)                 \
+{                                        \
+    .iface = SNDRV_CTL_ELEM_IFACE_MIXER, \
+    .name  = xname,                      \
+    .info  = cco_iobox_info,             \
+    .get   = cco_iobox_get,              \
+    .put   = cco_iobox_put,              \
+}
+/*============================================================================*/
+
 static const struct snd_kcontrol_new cco_controls[] = {
-CCO_VOLUME("Master Volume", 0, MIXER_ADDR_MASTER),
-CCO_CAPSRC("Master Capture Switch", 0, MIXER_ADDR_MASTER),
-CCO_VOLUME("Synth Volume", 0, MIXER_ADDR_SYNTH),
-CCO_CAPSRC("Synth Capture Switch", 0, MIXER_ADDR_SYNTH),
-CCO_VOLUME("Line Volume", 0, MIXER_ADDR_LINE),
-CCO_CAPSRC("Line Capture Switch", 0, MIXER_ADDR_LINE),
-CCO_VOLUME("Mic Volume", 0, MIXER_ADDR_MIC),
-CCO_CAPSRC("Mic Capture Switch", 0, MIXER_ADDR_MIC),
-CCO_VOLUME("CD Volume", 0, MIXER_ADDR_CD),
-CCO_CAPSRC("CD Capture Switch", 0, MIXER_ADDR_CD),
-{
-    .iface = SNDRV_CTL_ELEM_IFACE_MIXER,
-    .name  = "External I/O Box",
-    .info  = cco_iobox_info,
-    .get   = cco_iobox_get,
-    .put   = cco_iobox_put,
-},
+    CCO_VOLUME("Master Volume",         0, MIXER_ADDR_MASTER),
+    CCO_CAPSRC("Master Capture Switch", 0, MIXER_ADDR_MASTER),
+    CCO_VOLUME("Synth Volume",          0, MIXER_ADDR_SYNTH),
+    CCO_CAPSRC("Synth Capture Switch",  0, MIXER_ADDR_SYNTH),
+    CCO_VOLUME("Line Volume",           0, MIXER_ADDR_LINE),
+    CCO_CAPSRC("Line Capture Switch",   0, MIXER_ADDR_LINE),
+    CCO_VOLUME("Mic Volume",            0, MIXER_ADDR_MIC),
+    CCO_CAPSRC("Mic Capture Switch",    0, MIXER_ADDR_MIC),
+    CCO_VOLUME("CD Volume",             0, MIXER_ADDR_CD),
+    CCO_CAPSRC("CD Capture Switch",     0, MIXER_ADDR_CD),
+    CCO_IOBOX("External I/O Box"),
 };
 
 int cco_mixer_init(struct cco_device *cco)
@@ -195,7 +215,7 @@ int cco_mixer_init(struct cco_device *cco)
             cco->cd_volume_ctl = kcontrol;
         else if (!strcmp(kcontrol->id.name, "CD Capture Switch"))
             cco->cd_switch_ctl = kcontrol;
-
     }
+
     return 0;
 }
