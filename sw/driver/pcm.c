ce61d45105c71d1a25e9b634df272c6f34cb2092
Jake Whitton <jwhitton@alum.mit.edu>
2024-09-17 16:01:01 -0400

    Refactor timer logic into pcm.c in driver

--- sw/driver/pcm.c
+++ sw/driver/pcm.c
@@ -1,36 +1,37 @@
 #include "pcm.h"
 
 #include "log.h"
-#include "timer.h"
+
+/*===============================Initialization===============================*/
+// Full definition is in "PCM interface" section
+static const struct snd_pcm_ops cco_pcm_ops;
 
 static void *page[2];
 
-/*================================PCM interface===============================*/
-static int cco_pcm_open(struct snd_pcm_substream *substream)
+int cco_pcm_init(struct cco_device *cco, int device, int substreams)
 {
     int err;
 
-    const struct cco_timer_ops *ops = &cco_systimer_ops;
-
-    // Allocates a `struct cco_systimer_pcm` and smuggles it into private_data of
-    // subsystem->runtime.  Also initializes timer state and kicks off timer.
-    err = ops->create(substream);
-    if (err < 0)
+    struct snd_pcm *pcm;
+    err = snd_pcm_new(
+        cco->card, /* snd_card instance */
+        "CCO PCM", /* id */
+        device,    /* device number */
+        substreams /* playback_count */,
+        substreams /* capture_count */,
+        &pcm);     /* pcm intance */
+    if (err < 0) {
+        printk(KERN_ERR "cco: snd_pcm_new() failed\n");
         goto exit_error;
-
-    // Only overwrites `cco_systimer_pcm.timer_ops`, due to downcasting to
-    // `cco_timer_ops *` that get_cco_ops() performs.  Does not overwrite the
-    // rest of fields in cco_systimer_pcm
-    get_cco_ops(substream) = ops;
-
-    struct snd_pcm_runtime *runtime = substream->runtime;
-    runtime->hw = cco_pcm_hardware;
-    if (substream->pcm->device & 1) {
-        runtime->hw.info &= ~SNDRV_PCM_INFO_INTERLEAVED;
-        runtime->hw.info |= SNDRV_PCM_INFO_NONINTERLEAVED;
     }
-    if (substream->pcm->device & 2)
-        runtime->hw.info &= ~(SNDRV_PCM_INFO_MMAP | SNDRV_PCM_INFO_MMAP_VALID);
+    pcm->info_flags = 0;
+    strcpy(pcm->name, "CCO PCM");
+
+    // Sound core will propagate to snd_pcm_substream->private_data
+    pcm->private_data = cco;
+
+    snd_pcm_set_ops(pcm, SNDRV_PCM_STREAM_PLAYBACK, &cco_pcm_ops);
+    snd_pcm_set_ops(pcm, SNDRV_PCM_STREAM_CAPTURE, &cco_pcm_ops);
 
     return 0;
 
@@ -39,79 +40,6 @@ exit_error:
     return err;
 }
 
-static int cco_pcm_close(struct snd_pcm_substream *substream)
-{
-    get_cco_ops(substream)->free(substream);
-    return 0;
-}
-
-static int cco_pcm_hw_params(struct snd_pcm_substream *substream,
-                             struct snd_pcm_hw_params *hw_params)
-{
-    /* runtime->dma_bytes has to be set manually to allow mmap */
-    substream->runtime->dma_bytes = params_buffer_bytes(hw_params);
-    return 0;
-}
-
-static int cco_pcm_prepare(struct snd_pcm_substream *substream)
-{
-    return get_cco_ops(substream)->prepare(substream);
-}
-
-static int cco_pcm_trigger(struct snd_pcm_substream *substream, int cmd)
-{
-    switch (cmd) {
-        case SNDRV_PCM_TRIGGER_START:
-        case SNDRV_PCM_TRIGGER_RESUME:
-            return get_cco_ops(substream)->start(substream);
-        case SNDRV_PCM_TRIGGER_STOP:
-        case SNDRV_PCM_TRIGGER_SUSPEND:
-            return get_cco_ops(substream)->stop(substream);
-    }
-
-    return -EINVAL;
-}
-
-static snd_pcm_uframes_t cco_pcm_pointer(struct snd_pcm_substream *substream)
-{
-    return get_cco_ops(substream)->pointer(substream);
-}
-
-static int cco_pcm_copy(struct snd_pcm_substream *substream,
-                        int channel, unsigned long pos,
-                        struct iov_iter *iter, unsigned long bytes)
-{
-    return 0; /* do nothing */
-}
-
-static int cco_pcm_silence(struct snd_pcm_substream *substream,
-                           int channel, unsigned long pos,
-                           unsigned long bytes)
-{
-    return 0; /* do nothing */
-}
-
-static struct page *cco_pcm_page(struct snd_pcm_substream *substream,
-                                 unsigned long offset)
-{
-    return virt_to_page(page[substream->stream]); /* the same page */
-}
-
-static const struct snd_pcm_ops cco_pcm_ops = {
-    .open         = cco_pcm_open,
-    .close        = cco_pcm_close,
-    .hw_params    = cco_pcm_hw_params,
-    .prepare      = cco_pcm_prepare,
-    .trigger      = cco_pcm_trigger,
-    .pointer      = cco_pcm_pointer,
-    .copy         = cco_pcm_copy,
-    .fill_silence = cco_pcm_silence,
-    .page         = cco_pcm_page,
-};
-/*============================================================================*/
-
-
-/*===============================Initialization===============================*/
 int alloc_fake_buffer(void)
 {
     int err;
@@ -141,35 +69,220 @@ void free_fake_buffer(void)
         }
     }
 }
+/*============================================================================*/
 
-int cco_pcm_init(struct cco_device *cco, int device, int substreams)
+
+/*================================PCM interface===============================*/
+// State to be allocated per-substream
+struct cco_pcm_impl {
+    // Misc state
+    spinlock_t lock;
+    struct timer_list timer;
+    struct snd_pcm_substream *substream;
+
+    // Buffer state
+    unsigned long base_time;
+    unsigned int frac_pos;         /* fractional sample position (based HZ) */
+    unsigned int frac_period_rest;
+    unsigned int frac_buffer_size; /* buffer_size * HZ */
+    unsigned int frac_period_size; /* period_size * HZ */
+    unsigned int rate;
+    int elapsed;
+};
+
+// Defined in "Timer handling" section
+static void cco_pcm_timer_callback(struct timer_list *t);
+static void cco_pcm_timer_rearm(struct cco_pcm_impl *impl);
+static void cco_pcm_timer_update(struct cco_pcm_impl *impl);
+
+static int cco_pcm_open(struct snd_pcm_substream *substream)
 {
     int err;
 
-    struct snd_pcm *pcm;
-    err = snd_pcm_new(
-        cco->card, /* snd_card instance */
-        "CCO PCM", /* id */
-        device,    /* device number */
-        substreams /* playback_count */,
-        substreams /* capture_count */,
-        &pcm);     /* pcm intance */
-    if (err < 0) {
-        printk(KERN_ERR "cco: snd_pcm_new() failed\n");
+    // Allocate and initialize state for handling newly created substream
+    struct cco_pcm_impl *impl;
+    impl = kzalloc(sizeof(*impl), GFP_KERNEL);
+    if (!impl) {
+        err = -ENOMEM;
         goto exit_error;
     }
+    impl->substream = substream;
+    spin_lock_init(&impl->lock);
+    timer_setup(&impl->timer, cco_pcm_timer_callback, 0);
 
-    pcm->info_flags = 0;
-    strcpy(pcm->name, "CCO PCM");
-    pcm->private_data = cco;
-
-    snd_pcm_set_ops(pcm, SNDRV_PCM_STREAM_PLAYBACK, &cco_pcm_ops);
-    snd_pcm_set_ops(pcm, SNDRV_PCM_STREAM_CAPTURE, &cco_pcm_ops);
+    struct snd_pcm_runtime *runtime = substream->runtime;
+    runtime->private_data = impl;
+    runtime->hw = cco_pcm_hardware;
+    if (substream->pcm->device & 1) {
+        runtime->hw.info &= ~SNDRV_PCM_INFO_INTERLEAVED;
+        runtime->hw.info |= SNDRV_PCM_INFO_NONINTERLEAVED;
+    }
+    if (substream->pcm->device & 2)
+        runtime->hw.info &= ~(SNDRV_PCM_INFO_MMAP | SNDRV_PCM_INFO_MMAP_VALID);
 
     return 0;
 
 exit_error:
     CCO_LOG_FUNCTION_FAILURE(err);
     return err;
+}
+
+static int cco_pcm_close(struct snd_pcm_substream *substream)
+{
+    kfree(substream->runtime->private_data);
+    return 0;
+}
+
+static int cco_pcm_hw_params(struct snd_pcm_substream *substream,
+                             struct snd_pcm_hw_params *hw_params)
+{
+    /* runtime->dma_bytes has to be set manually to allow mmap */
+    substream->runtime->dma_bytes = params_buffer_bytes(hw_params);
+    return 0;
+}
+
+static int cco_pcm_prepare(struct snd_pcm_substream *substream)
+{
+    struct snd_pcm_runtime *runtime = substream->runtime;
+    struct cco_pcm_impl *impl = runtime->private_data;
+
+    impl->frac_pos = 0;
+    impl->rate = runtime->rate;
+    impl->frac_buffer_size = runtime->buffer_size * HZ;
+    impl->frac_period_size = runtime->period_size * HZ;
+    impl->frac_period_rest = impl->frac_period_size;
+    impl->elapsed = 0;
+
+    return 0;
+}
+
+static int cco_pcm_trigger(struct snd_pcm_substream *substream, int cmd)
+{
+    int err;
+    struct cco_pcm_impl *impl = substream->runtime->private_data;
+
+    switch (cmd) {
+        case SNDRV_PCM_TRIGGER_START:
+        case SNDRV_PCM_TRIGGER_RESUME:
+            spin_lock(&impl->lock);
+            impl->base_time = jiffies;
+            cco_pcm_timer_rearm(impl);
+            spin_unlock(&impl->lock);
+            break;
+
+
+        case SNDRV_PCM_TRIGGER_STOP:
+        case SNDRV_PCM_TRIGGER_SUSPEND:
+
+            spin_lock(&impl->lock);
+            del_timer(&impl->timer);
+            spin_unlock(&impl->lock);
+            break;
+
+        default:
+            err = -EINVAL;
+            goto exit_error;
+    }
+
+    return 0;
+
+exit_error:
+    CCO_LOG_FUNCTION_FAILURE(err);
+    return err;
+}
+
+static snd_pcm_uframes_t cco_pcm_pointer(struct snd_pcm_substream *substream)
+{
+    snd_pcm_uframes_t pos;
+    struct cco_pcm_impl *impl = substream->runtime->private_data;
+
+    spin_lock(&impl->lock);
+    cco_pcm_timer_update(impl);
+    pos = impl->frac_pos / HZ;
+    spin_unlock(&impl->lock);
+
+    return pos;
+}
+
+static int cco_pcm_silence(struct snd_pcm_substream *substream,
+                           int channel, unsigned long pos,
+                           unsigned long bytes)
+{
+    return 0; /* do nothing */
+}
+
+static int cco_pcm_copy(struct snd_pcm_substream *substream,
+                        int channel, unsigned long pos,
+                        struct iov_iter *iter, unsigned long bytes)
+{
+    return 0; /* do nothing */
+}
+
+static struct page *cco_pcm_page(struct snd_pcm_substream *substream,
+                                 unsigned long offset)
+{
+    return virt_to_page(page[substream->stream]); /* the same page */
+}
+
+static const struct snd_pcm_ops cco_pcm_ops = {
+    .open         = cco_pcm_open,
+    .close        = cco_pcm_close,
+    .hw_params    = cco_pcm_hw_params,
+    .prepare      = cco_pcm_prepare,
+    .trigger      = cco_pcm_trigger,
+    .pointer      = cco_pcm_pointer,
+    .fill_silence = cco_pcm_silence,
+    .copy         = cco_pcm_copy,
+    .page         = cco_pcm_page,
+};
+/*============================================================================*/
+
+
+/*===============================Timer handling===============================*/
+static void cco_pcm_timer_callback(struct timer_list *t)
+{
+    struct cco_pcm_impl *impl = from_timer(impl, t, timer);
+    unsigned long flags;
+    int elapsed = 0;
+
+    spin_lock_irqsave(&impl->lock, flags);
+    cco_pcm_timer_update(impl);
+    cco_pcm_timer_rearm(impl);
+    elapsed = impl->elapsed;
+    impl->elapsed = 0;
+    spin_unlock_irqrestore(&impl->lock, flags);
+
+    if (elapsed)
+        snd_pcm_period_elapsed(impl->substream);
+}
+
+static void cco_pcm_timer_rearm(struct cco_pcm_impl *impl)
+{
+    mod_timer(&impl->timer, jiffies +
+              DIV_ROUND_UP(impl->frac_period_rest, impl->rate));
+}
+
+static void cco_pcm_timer_update(struct cco_pcm_impl *impl)
+{
+    unsigned long delta;
+
+    delta = jiffies - impl->base_time;
+    if (!delta)
+        return;
+
+    impl->base_time += delta;
+    delta *= impl->rate;
+    impl->frac_pos += delta;
+
+    while (impl->frac_pos >= impl->frac_buffer_size) {
+        impl->frac_pos -= impl->frac_buffer_size;
+    }
+
+    while (impl->frac_period_rest <= delta) {
+        impl->elapsed++;
+        impl->frac_period_rest += impl->frac_period_size;
+    }
+
+    impl->frac_period_rest -= delta;
 }
 /*============================================================================*/
