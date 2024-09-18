0fddc092b8b793f17e0e4e8a37f8b528ce3a73ae
Jake Whitton <jwhitton@alum.mit.edu>
2024-09-17 22:10:23 -0400

    Add kernel driver sources

---
+++ sw/driver/device.h
@@ -0,0 +1,30 @@
+#ifndef CCO_DEVICE_H
+#define CCO_DEVICE_H
+
+#include <linux/platform_device.h>
+#include <sound/core.h>
+
+#include "mixer.h"
+
+#define PCM_DEVICES_PER_CARD      1
+#define PCM_SUBSTREAMS_PER_DEVICE 2
+
+struct cco_device {
+    struct platform_device pdev;
+    struct snd_card *card;
+    struct cco_mixer mixer;
+};
+
+#define pdev_to_cco(pdev) container_of((pdev), struct cco_device, pdev)
+#define dev_to_cco(dev) container_of((dev), struct cco_device, pdev.dev)
+
+// Driver management
+int cco_register_driver(void);
+void cco_unregister_driver(void);
+
+// Device management
+int cco_register_device(void);
+void cco_unregister_device(int id);
+void cco_unregister_devices(void);
+
+#endif
